557d265464d46f0c84867792bc950a1a4f5da3b8
Nathanael Tan <nathanaeltan@nathanaels-mbp.lan>
2019-07-23 14:32:21 +0800

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,6 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+
+
+cardsInPlay.push(cardOne);
+console.log("user flipped " + cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardsInPlay[1]);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match")
+} else {
+    alert("sorry try again");
+}
