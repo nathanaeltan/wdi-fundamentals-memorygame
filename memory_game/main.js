b3c908e372a1ad7400e9fe6bd26d8ead8900a135
Nathanael Tan <nathanaeltan@nathanaels-mbp.lan>
2019-07-24 11:20:51 +0800

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,20 +1,47 @@
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+
+]
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match");
+    alert("You found a match");
   } else {
-    console.log("sorry try again");
+    alert("sorry try again");
   }
+  checkForMatch();
 };
 
 var flipCard = function(cardId) {
-  console.log("User Flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User Flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+  
   
 };
 flipCard(0);
 flipCard(2);
-checkForMatch();
+
+
