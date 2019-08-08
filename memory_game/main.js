1f91e620a6d014075427e9fd7d1f7c496cf377ab
Nathanael Tan <nathanaeltan@nathanaels-mbp.lan>
2019-08-08 12:02:09 +0800

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,3 +1,6 @@
+var userScore = 0;
+var user_score = document.getElementById('user-score')
+
 var cards = [
     {
         rank: "queen",
@@ -22,26 +25,49 @@ var cards = [
 
 ]
 
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match");
+    userScore++;
+    user_score.innerHTML = userScore;
+
   } else {
     alert("sorry try again");
   }
-  checkForMatch();
+ 
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id')
   console.log("User Flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
+
+  this.setAttribute('src', cards[cardId].cardImage);
+ if (cardsInPlay.length === 2){
+    checkForMatch();
   
-  
+ };
 };
-flipCard(0);
-flipCard(2);
 
 
+var createBoard = function() {
+  for(var i = 0; i< cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard)
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+var reset = function() {
+  rbtn = document.getElementById('reset');
+}
+
+createBoard();
